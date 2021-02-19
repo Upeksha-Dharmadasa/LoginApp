909a09c78c75793a92132c6dfaa903947d64d63c
Upeksha Dharmadasa <iudharmadasa@icloud.com>
2021-02-19 08:27:22 +0530

    Initial Commit

---
+++ LoginApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LoginApp
+//
+//  Created by Upeksha Dharmadasa on 2/19/21.
+//  Copyright © 2021 Upeksha Dharmadasa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
